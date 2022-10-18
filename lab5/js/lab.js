c07b406732248432a152fd10f66bb0df89e6dc60
DanielZehaoDong <zdong18@ucsc.edu>
2022-10-18 04:01:21 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,17 @@
+/**
+ * Author:    Daniel, Noctis
+ * Created:   10.18.2022
+ * license:   Public Domain
+**/
+
+make = "BMW";
+model = "328i";
+color = "white";
+year = 2013;
+age = 2022 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
